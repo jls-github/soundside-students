b7cbf99fda9c6641fdf5893c96615ec2a1cc2186
github username <souzajohnlee@gmail.com>
2021-02-13 13:48:19 -0800

    create api to fetch newsletters

--- pages/api/newsletters_api.tsx
+++ pages/api/newsletters_api.tsx
@@ -1,14 +1,25 @@
-import {join} from 'path'
-import fs from 'fs'
+import { join } from "path";
+import fs from "fs";
+import matter from 'gray-matter';
 
-export {}
+export {};
 
-const newslettersDirectory: string = join(process.cwd(), '_newsletters')
+const newslettersDirectory: string = join(process.cwd(), "_newsletters");
 
-// export const getNewsletters: Promise<{slug: string, title: string}[]> {
+export const getNewsletterPaths = (): string[] => {
+  return fs.readdirSync(newslettersDirectory);
+};
 
-// }
-
-export const getPostSlugs: string[] {
-    return fs.readdirSync(newslettersDirectory)
+export const getNewsletterByPath = (path: string): {slug: string, title: string} => {
+    const slug = path.replace(/\.md$/, '');
+    const fileContents = fs.readFileSync(path, 'utf8');
+    const {data} = matter(fileContents)
+    const newsletter = {slug: slug, title: data["title"]}
+    return newsletter
 }
+
+export const getNewsletters = (): { slug: string, title: string }[] => {
+  const paths = getNewsletterPaths();
+  const newsletters = paths.map((path) => getNewsletterByPath(path))
+  return newsletters;
+};
